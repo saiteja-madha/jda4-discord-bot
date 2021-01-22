76bb07dd77af6643f7bce396f734b3c32972ec26
Sai Teja Madha <42540377+saiteja-madha@users.noreply.github.com>
2021-01-23 01:49:10 +0530

    flipcoin, fliptext, translate, urban

---
+++ src/main/java/bot/command/commands/fun/FlipCoinCommand.java
@@ -0,0 +1,34 @@
+package bot.command.commands.fun;
+
+import bot.command.CommandContext;
+import bot.command.ICommand;
+import me.duncte123.botcommons.messaging.EmbedUtils;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
+
+public class FlipCoinCommand extends ICommand {
+
+    public FlipCoinCommand() {
+        this.name = "flipcoin";
+        this.usage = "flips a coin heads or tails";
+        this.botPermissions = new Permission[]{Permission.MESSAGE_EMBED_LINKS};
+    }
+
+    @Override
+    public void handle(@NotNull CommandContext ctx) {
+        boolean isHeads = new Random().nextBoolean();
+
+        String HEADS = "https://i.imgur.com/HavOS7J.png";
+        String TAILS = "https://i.imgur.com/u1pmQMV.png";
+
+        EmbedBuilder embed = EmbedUtils.defaultEmbed()
+                .setAuthor(ctx.getAuthor().getAsTag(), null, ctx.getAuthor().getEffectiveAvatarUrl())
+                .setImage(isHeads ? HEADS : TAILS);
+
+        ctx.reply(embed.build());
+
+    }
+}
