60eaf45d50d1929eeefb0a6295c427cc41216e22
Sai Teja Madha <42540377+saiteja-madha@users.noreply.github.com>
2021-03-01 11:32:43 +0530

    fixes

--- src/main/java/bot/commands/utility/HelpCommand.java
+++ src/main/java/bot/commands/utility/HelpCommand.java
@@ -41,7 +41,10 @@ public class HelpCommand extends ICommand {
             str += Constants.ARROW + " image" + "\n";
             str += Constants.ARROW + " information" + "\n";
             str += Constants.ARROW + " social" + "\n";
-            str += Constants.ARROW + " moderation" + "\n";
+            str += Constants.ARROW + " moderation" + "\n\n";
+            str += "**Admin Modules:**" + "\n";
+            str += Constants.ARROW + " admin" + "\n";
+            str += Constants.ARROW + " automod" + "\n";
 
             EmbedBuilder embed = EmbedUtils.getDefaultEmbed()
                     .setDescription(str)
