6f0068bddc8c5bbac310f56e3394b63a09bc425f
Sai Teja Madha <42540377+saiteja-madha@users.noreply.github.com>
2021-03-14 15:11:41 +0530

    invites rank, invitecodes commands

---
+++ src/main/java/bot/commands/invites/InviteCodesCommand.java
@@ -0,0 +1,74 @@
+package bot.commands.invites;
+
+import bot.command.CommandCategory;
+import bot.command.CommandContext;
+import bot.command.ICommand;
+import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
+import com.jagrosh.jdautilities.menu.Paginator;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Invite;
+import net.dv8tion.jda.api.entities.Member;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InviteCodesCommand extends ICommand {
+
+    private final Paginator.Builder pBuilder;
+
+    public InviteCodesCommand(EventWaiter waiter) {
+        this.name = "invitecodes";
+        this.help = "list all your invites codes in this guild";
+        this.aliases = Collections.singletonList("invite-codes");
+        this.userPermissions = new Permission[]{Permission.MANAGE_SERVER};
+        this.category = CommandCategory.INVITES;
+        this.pBuilder = new Paginator.Builder()
+                .setColumns(1)
+                .setItemsPerPage(5)
+                .setFinalAction(m -> {
+                })
+                .setEventWaiter(waiter);
+    }
+
+    @Override
+    public void handle(@NotNull CommandContext ctx) {
+        final List<Member> menMembers = ctx.getMessage().getMentionedMembers();
+        Member target = ctx.getMember();
+
+        if (!menMembers.isEmpty()) {
+            target = menMembers.get(0);
+        }
+
+        final Member finalTarget = target;
+        ctx.getGuild().retrieveInvites().queue(invites -> {
+            final List<Invite> filter = invites.stream()
+                    .filter(invite -> (invite.getInviter() != null
+                            && invite.getInviter().getId().equalsIgnoreCase(finalTarget.getId()))
+                    )
+                    .collect(Collectors.toList());
+
+            if (filter.isEmpty()) {
+                ctx.reply("You do not have invite codes in this server");
+                return;
+            }
+
+            pBuilder.clearItems();
+            filter.forEach((invite) ->
+                    pBuilder.addItems("\u276F" + " [" + invite.getCode() + "](" + invite.getUrl() + ") : " + invite.getUses() + " uses")
+            );
+
+            Paginator p = pBuilder
+                    .addUsers(ctx.getAuthor())
+                    .setText("Invite Codes for **" + finalTarget.getEffectiveName() + "**")
+                    .setColor(Color.decode(String.valueOf(0x3883d9)))
+                    .build();
+
+            p.paginate(ctx.getChannel(), 1);
+
+        });
+    }
+
+}
---
+++ src/main/java/bot/commands/invites/InvitesRankCommand.java
@@ -0,0 +1,57 @@
+package bot.commands.invites;
+
+import bot.command.CommandCategory;
+import bot.command.CommandContext;
+import bot.command.ICommand;
+import bot.database.DataSource;
+import com.jagrosh.jdautilities.commons.utils.FinderUtil;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Role;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class InvitesRankCommand extends ICommand {
+
+    public InvitesRankCommand() {
+        this.name = "addinviterank";
+        this.help = "add auto-rank after reaching a particular number of invites";
+        this.usage = "<@role> <invites>";
+        this.minArgsCount = 2;
+        this.userPermissions = new Permission[]{Permission.MANAGE_SERVER};
+        this.category = CommandCategory.INVITES;
+    }
+
+    @Override
+    public void handle(@NotNull CommandContext ctx) {
+        Role role = null;
+        final List<Role> menRoles = ctx.getMessage().getMentionedRoles();
+
+        if (!menRoles.isEmpty())
+            role = menRoles.get(0);
+
+        if (role == null) {
+            String roleName = ctx.getArgs().get(0);
+            final List<Role> roles = FinderUtil.findRoles(roleName, ctx.getGuild());
+            if (!roles.isEmpty()) {
+                role = roles.get(0);
+            }
+        }
+
+        if (role == null) {
+            ctx.reply("No roles found matching `" + ctx.getArgs().get(0) + "");
+            return;
+        }
+
+        final String invites = ctx.getArgs().get(1);
+        try {
+            int inviteCount = Integer.parseInt(invites);
+            DataSource.INS.addInvitesRank(ctx.getGuildId(), role.getId(), inviteCount);
+            ctx.reply("Success! Configuration saved");
+        } catch (NumberFormatException ex) {
+            ctx.reply("Did you provide a valid number of invites?");
+        }
+
+    }
+
+}
