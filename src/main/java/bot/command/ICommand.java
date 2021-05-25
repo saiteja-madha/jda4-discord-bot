6374c6b240943b2e0eda96eb9279d4c42c5f5df8
Sai Teja Madha <42540377+saiteja-madha@users.noreply.github.com>
2021-05-25 15:51:38 +0530

    format time

--- src/main/java/bot/command/ICommand.java
+++ src/main/java/bot/command/ICommand.java
@@ -1,6 +1,7 @@
 package bot.command;
 
 import bot.utils.BotUtils;
+import bot.utils.MiscUtils;
 import me.duncte123.botcommons.messaging.EmbedUtils;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
@@ -137,7 +138,7 @@ public abstract class ICommand {
         BotUtils.sendEmbed(
                 event.getChannel(),
                 EmbedUtils.getDefaultEmbed()
-                        .setAuthor("Kindly wait " + seconds + " seconds before using this command")
+                        .setAuthor("Kindly wait `" + MiscUtils.formatTime(seconds) + "` before using this command")
                         .build(),
                 5
         );
